e8436ba776a018e100e203ce1986d476d9181409
Jhonah <buryakone@gmail.com>
2021-06-27 19:34:44 +0700

    запоминание уникальных анаграмм через коллекцию Map

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
 alert(aclean(arr)); // "nap,teachers,ear" or "PAN,cheaters,era"
 
 function aclean(anagrams) {
-    let sortSet = new Set();
-    let set = new Set();
+
+    let map = new Map();
+
     for (let elem of anagrams) {
-        let sortElem = Array.from(elem)
+        let key = Array.from(elem)
             .sort((a, b) => a > b ? 1 : -1)
             .join('')
             .toLocaleLowerCase();
 
-        if (!sortSet.has(sortElem)) {
-            sortSet.add(sortElem);
-            set.add(elem);
-        }
+        //if (!map.has(key)) {
+            map.set(key, elem);
+        //}
     }
-    
-    return Array.from(set);
+
+    return Array.from(map.values());    
 }
